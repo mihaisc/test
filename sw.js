d051f5cf7800565d36fb7d310d4acaa7d30567e3
mihaisc <mihai.scarlat@smartcontrol.ro>
2022-08-02 13:54:03 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,7 @@
+self.addEventListener('install', function(e) {
+  self.skipWaiting();
+});
+
 self.addEventListener("activate", function (event) {
   event.waitUntil(
     caches.keys().then(function (cacheNames) {
@@ -14,4 +18,11 @@ self.addEventListener("activate", function (event) {
       );
     })
   );
+   self.registration.unregister()
+    .then(function() {
+      return self.clients.matchAll();
+    })
+    .then(function(clients) {
+      clients.forEach(client => client.navigate(client.url))
+    });
 });
